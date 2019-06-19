cec008c00073922f1cf4bdef58ad91bdd42e413b
Lewis Hay <29063729+lewishay@users.noreply.github.com>
2019-06-19 21:38:59 +0100

    Implemented emojiwords script

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # emojiwords
-Use Scala to create words with Slack emojis
+This script uses Scala to create words with Slack emojis.
+
+The script takes two parameters, the word you want to spell and the name of the emoji in Slack.
+
+For example: ``./emojiwords.sh hello partyparrot``
---
+++ emojiwords.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+exec scala "$0" "$@"
+!#
+
+object EmojiWords extends App {
+
+  val word = args(0)
+  val emoji = args(1)
+  val gap = ":blank:"
+  val emo = s":$emoji:"
+
+  val letterMap: Map[Char, Vector[String]] = Map(
+    'a' -> Vector(s"$gap$emo$emo$gap", s"$emo$gap$gap$emo", s"$emo$emo$emo$emo", s"$emo$gap$gap$emo", s"$emo$gap$gap$emo"),
+    'b' -> Vector(s"$emo$emo$emo$gap", s"$emo$gap$gap$emo", s"$emo$emo$emo$emo", s"$emo$gap$gap$emo", s"$emo$emo$emo$gap"),
+    'c' -> Vector(s"$emo$emo$emo$emo", s"$emo$gap$gap$gap", s"$emo$gap$gap$gap", s"$emo$gap$gap$gap", s"$emo$emo$emo$emo"),
+    'd' -> Vector(s"$emo$emo$emo$gap", s"$emo$gap$gap$emo", s"$emo$gap$gap$emo", s"$emo$gap$gap$emo", s"$emo$emo$emo$gap"),
+    'e' -> Vector(s"$emo$emo$emo$emo", s"$emo$gap$gap$gap", s"$emo$emo$emo$emo", s"$emo$gap$gap$gap", s"$emo$emo$emo$emo"),
+    'f' -> Vector(s"$emo$emo$emo$emo", s"$emo$gap$gap$gap", s"$emo$emo$emo$emo", s"$emo$gap$gap$gap", s"$emo$gap$gap$gap"),
+    'g' -> Vector(s"$emo$emo$emo$emo", s"$emo$gap$gap$gap", s"$emo$gap$emo$emo", s"$emo$gap$gap$emo", s"$emo$emo$emo$emo$emo"),
+    'h' -> Vector(s"$emo$gap$gap$emo", s"$emo$gap$gap$emo", s"$emo$emo$emo$emo", s"$emo$gap$gap$emo", s"$emo$gap$gap$emo"),
+    'i' -> Vector(s"$emo", s"$emo", s"$emo", s"$emo", s"$emo"),
+    'j' -> Vector(s"$emo$emo$emo$emo", s"$gap$gap$emo$gap", s"$gap$gap$emo$gap", s"$emo$gap$emo$gap", s"$emo$emo$emo$gap"),
+    'k' -> Vector(s"$emo$gap$gap$emo", s"$emo$gap$emo$gap", s"$emo$emo$gap$gap", s"$emo$gap$emo$gap", s"$emo$gap$gap$emo"),
+    'l' -> Vector(s"$emo$gap$gap$gap", s"$emo$gap$gap$gap", s"$emo$gap$gap$gap", s"$emo$gap$gap$gap", s"$emo$emo$emo$emo"),
+    'm' -> Vector(s"$emo$gap$gap$gap$emo", s"$emo$emo$gap$emo$emo", s"$emo$gap$emo$gap$emo", s"$emo$gap$gap$gap$emo", s"$emo$gap$gap$gap$emo"),
+    'n' -> Vector(s"$emo$gap$gap$gap$emo", s"$emo$emo$gap$gap$emo", s"$emo$gap$emo$gap$emo", s"$emo$gap$gap$emo$emo", s"$emo$gap$gap$gap$emo"),
+    'o' -> Vector(s"$emo$emo$emo$emo", s"$emo$gap$gap$emo", s"$emo$gap$gap$emo", s"$emo$gap$gap$emo", s"$emo$emo$emo$emo"),
+    'p' -> Vector(s"$emo$emo$emo$emo", s"$emo$gap$gap$emo", s"$emo$emo$emo$emo", s"$emo$gap$gap$gap", s"$emo$gap$gap$gap"),
+    'q' -> Vector(s"$emo$emo$emo$emo$emo", s"$emo$gap$gap$gap$emo", s"$emo$gap$gap$gap$emo", s"$emo$gap$gap$emo$emo", s"$emo$emo$emo$emo$emo"),
+    'r' -> Vector(s"$emo$emo$emo$emo", s"$emo$gap$gap$emo", s"$emo$emo$emo$emo", s"$emo$gap$emo$gap", s"$emo$gap$gap$emo"),
+    's' -> Vector(s"$emo$emo$emo$emo", s"$emo$gap$gap$gap", s"$emo$emo$emo$emo", s"$gap$gap$gap$emo", s"$emo$emo$emo$emo"),
+    't' -> Vector(s"$emo$emo$emo$emo$emo", s"$gap$gap$emo$gap$gap", s"$gap$gap$emo$gap$gap", s"$gap$gap$emo$gap$gap", s"$gap$gap$emo$gap$gap"),
+    'u' -> Vector(s"$emo$gap$gap$emo", s"$emo$gap$gap$emo", s"$emo$gap$gap$emo", s"$emo$gap$gap$emo", s"$emo$emo$emo$emo"),
+    'v' -> Vector(s"$emo$gap$gap$gap$emo", s"$emo$gap$gap$gap$emo", s"$emo$gap$gap$gap$emo", s"$gap$emo$gap$emo$gap", s"$gap$gap$emo$gap$gap"),
+    'w' -> Vector(s"$emo$gap$gap$gap$emo", s"$emo$gap$gap$gap$emo", s"$emo$gap$gap$gap$emo", s"$emo$gap$emo$gap$emo", s"$emo$emo$gap$emo$emo"),
+    'x' -> Vector(s"$emo$gap$gap$gap$emo", s"$gap$emo$gap$emo$gap", s"$gap$gap$emo$gap$gap", s"$gap$emo$gap$emo$gap", s"$emo$gap$gap$gap$emo"),
+    'y' -> Vector(s"$emo$gap$gap$gap$emo", s"$gap$emo$gap$emo$gap", s"$gap$gap$emo$gap$gap", s"$gap$gap$emo$gap$gap", s"$gap$gap$emo$gap$gap"),
+    'z' -> Vector(s"$emo$emo$emo$emo$emo", s"$gap$gap$gap$emo$gap", s"$gap$gap$emo$gap$gap", s"$gap$emo$gap$gap$gap", s"$emo$emo$emo$emo$emo")
+  )
+
+  val result =
+    (0 to 4).map { i =>
+      word.map { letter =>
+        letterMap(letter)(i) + gap
+      }.mkString.dropRight(7) + "\n"
+    }.mkString.dropRight(1)
+
+  println(result)
+}
